c0c46a74ee5acb523ce2a20b7aa84fe4de04b797
Elvis Nuñez <elvisnunez@me.com>
2015-04-21 20:10:47 +0200

    Rename constants

--- Source/NSEntityDescription+HYPPrimaryKey.h
+++ Source/NSEntityDescription+HYPPrimaryKey.h
@@ -1,9 +1,9 @@
 @import CoreData;
 
-static NSString * const SyncDefaultLocalPrimaryKey = @"remoteID";
-static NSString * const SyncDefaultRemotePrimaryKey = @"id";
+static NSString * const HYPPrimaryKeyDefaultLocal = @"remoteID";
+static NSString * const HYPPrimaryKeyDefaultRemote = @"id";
 
-static NSString * const SyncCustomPrimaryKey = @"hyper.isPrimaryKey";
+static NSString * const SyncCustomLocalKey = @"hyper.isPrimaryKey";
 static NSString * const SyncCustomRemoteKey = @"hyper.remoteKey";
 
 @interface NSEntityDescription (HYPPrimaryKey)
