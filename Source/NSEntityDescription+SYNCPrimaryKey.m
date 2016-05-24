3aaff33ea57e7bdd5d2b89ce3d341ef077945b82
Elvis Nuñez <elvisnunez@me.com>
2016-05-24 19:05:03 +0200

    Improve performance by caching and safety with lock

--- Source/NSEntityDescription+SYNCPrimaryKey.m
+++ Source/NSEntityDescription+SYNCPrimaryKey.m
@@ -2,28 +2,75 @@
 
 #import "NSString+HYPNetworking.h"
 
+@interface NSEntityDescriptionPrimaryKeyStorage : NSObject
+
+@property (nonatomic, strong) NSMutableDictionary *storage;
+@property (nonatomic, strong) NSLock *lock;
+
+@end
+
+@implementation NSEntityDescriptionPrimaryKeyStorage
+
++ (instancetype)sharedInstance {
+    static dispatch_once_t once;
+    static NSEntityDescriptionPrimaryKeyStorage *sharedInstance;
+    dispatch_once(&once, ^{
+        sharedInstance = [self new];
+    });
+    return sharedInstance;
+}
+
+- (NSMutableDictionary *)storage {
+    if (!_storage) {
+        _storage = [NSMutableDictionary new];
+    }
+
+    return _storage;
+}
+
+- (NSLock *)lock {
+    if (!_lock) {
+        _lock = [NSLock new];
+    }
+
+    return _lock;
+}
+
+@end
+
 @implementation NSEntityDescription (SYNCPrimaryKey)
 
 - (nonnull NSAttributeDescription *)sync_primaryKeyAttribute {
-    __block NSAttributeDescription *primaryKeyAttribute;
+    __block NSAttributeDescription *primaryKeyAttribute = [[[NSEntityDescriptionPrimaryKeyStorage sharedInstance] storage] objectForKey:self.name];
+    if (primaryKeyAttribute) {
+        return primaryKeyAttribute;
+    } else {
+        [self.propertiesByName enumerateKeysAndObjectsUsingBlock:^(NSString *key,
+                                                                   NSAttributeDescription *attributeDescription,
+                                                                   BOOL *stop) {
+            NSString *isPrimaryKey = attributeDescription.userInfo[SYNCCustomLocalPrimaryKey];
+            BOOL hasCustomPrimaryKey = (isPrimaryKey &&
+                                        ([isPrimaryKey isEqualToString:SYNCCustomLocalPrimaryKeyValue] || [isPrimaryKey isEqualToString:SYNCCustomLocalPrimaryKeyAlternativeValue]) );
+            if (hasCustomPrimaryKey) {
+                primaryKeyAttribute = attributeDescription;
+                *stop = YES;
+            }
 
-    [self.propertiesByName enumerateKeysAndObjectsUsingBlock:^(NSString *key,
-                                                               NSAttributeDescription *attributeDescription,
-                                                               BOOL *stop) {
-        NSString *isPrimaryKey = attributeDescription.userInfo[SYNCCustomLocalPrimaryKey];
-        BOOL hasCustomPrimaryKey = (isPrimaryKey &&
-                                    ([isPrimaryKey isEqualToString:SYNCCustomLocalPrimaryKeyValue] || [isPrimaryKey isEqualToString:SYNCCustomLocalPrimaryKeyAlternativeValue]) );
-        if (hasCustomPrimaryKey) {
-            primaryKeyAttribute = attributeDescription;
-            *stop = YES;
+            if ([key isEqualToString:SYNCDefaultLocalPrimaryKey] || [key isEqualToString:SYNCDefaultLocalCompatiblePrimaryKey]) {
+                primaryKeyAttribute = attributeDescription;
+            }
+        }];
+
+        [[[NSEntityDescriptionPrimaryKeyStorage sharedInstance] lock] lock];
+        if (primaryKeyAttribute) {
+            [[[NSEntityDescriptionPrimaryKeyStorage sharedInstance] storage] setObject:primaryKeyAttribute forKey:self.name];
+        } else {
+            [[[NSEntityDescriptionPrimaryKeyStorage sharedInstance] storage] removeObjectForKey:self.name];
         }
+        [[[NSEntityDescriptionPrimaryKeyStorage sharedInstance] lock] unlock];
 
-        if ([key isEqualToString:SYNCDefaultLocalPrimaryKey] || [key isEqualToString:SYNCDefaultLocalCompatiblePrimaryKey]) {
-            primaryKeyAttribute = attributeDescription;
-        }
-    }];
-
-    return primaryKeyAttribute;
+        return primaryKeyAttribute;
+    }
 }
 
 - (nonnull NSString *)sync_localPrimaryKey {
